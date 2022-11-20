67d89a8c399e8dc8de56402e7b03757b5f608012
ventuscoder <shivankcode@gmail.com>
2022-11-20 11:55:52 +0530

    database cloud connect

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/sports-league');
+//mongoose.connect('mongodb://localhost/sports-league');
+
+mongoose.connect('mongodb+srv://adminsportleague:passwordsportleague@cluster0.dhvp799.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
+.then(function(){
+  console.log('Database Connected')
+})
+.catch(function(err){
+  console.log(err)
+})
 
 let schema = mongoose.Schema(
   {
